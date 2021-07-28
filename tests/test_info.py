ea23c5a648db77bb972a371966dfe4d1f5e62783
Becker Awqatty <bawqatty@mide.com>
2021-07-28 18:05:07 -0400

    converted tests to pytest format

--- tests/test_info.py
+++ tests/test_info.py
@@ -1,34 +1,37 @@
 from datetime import datetime, timedelta
 import os.path
-import unittest
 
+import pytest
 from idelib.importer import importFile
 from endaq.ide import info
 
 
-IDE_FILENAME = os.path.join(os.path.dirname(__file__), "test.ide")
+@pytest.fixture
+def dataset():
+    with importFile(os.path.join("tests", "test.ide")) as ds:
+        yield ds
 
 
-class TimeParseTest(unittest.TestCase):
+class TestTimeParse:
     """ Test the time parsing function.
     """
     def test_parse_time(self):
         """ Test basic handling of non-strings. """
-        self.assertEqual(info.parse_time(None), None)
-        self.assertEqual(info.parse_time(""), None)
-        self.assertEqual(info.parse_time(42), 42)
-        self.assertRaises(TypeError, lambda *x: info.parse_time([1]),
-                          "parse_time() accepted a bad type (list)")
+        assert info.parse_time(None) is None
+        assert info.parse_time("") is None
+        assert info.parse_time(42) == 42
+        with pytest.raises(TypeError):
+            info.parse_time([1])
 
 
     def test_parse_time_str(self):
         """ Test time string parsing. """
-        self.assertEqual(info.parse_time("11"), 11 * 10**6)
-        self.assertEqual(info.parse_time("22:11"), 1331000000)
-        self.assertEqual(info.parse_time("3:22:11"), 12131000000)
-        self.assertEqual(info.parse_time("1d 3:22:11"), 98531000000)
-        self.assertRaises(ValueError, lambda *x: info.parse_time("bogus"),
-                          "parse_time() accepted a bad string")
+        assert info.parse_time("11") == 11 * 10**6
+        assert info.parse_time("22:11") == 1331000000
+        assert info.parse_time("3:22:11") == 12131000000
+        assert info.parse_time("1d 3:22:11") == 98531000000
+        with pytest.raises(ValueError):
+            info.parse_time("bogus")
 
 
     def test_parse_time_datetime(self):
@@ -37,85 +40,75 @@ class TimeParseTest(unittest.TestCase):
         t2 = datetime(2021, 7, 7, 0, 0)
         td = timedelta(days=1, seconds=2345, microseconds=6789)
 
-        self.assertEqual(info.parse_time(td), td.total_seconds() * 10**6)
-        self.assertEqual(info.parse_time(t1 - t2), 127720.800752 * 10**6)
-        self.assertEqual(info.parse_time(t1, t2), 127720.800752 * 10**6)
+        assert info.parse_time(td) == td.total_seconds() * 10**6
+        assert info.parse_time(t1 - t2) == 127720.800752 * 10**6
+        assert info.parse_time(t1, t2) == 127720.800752 * 10**6
 
 
-class ChannelTableFormattingTests(unittest.TestCase):
+class TestChannelTableFormatting:
     """ Test the individual column value formatting functions.
     """
+    def test_format_channel_id(self, dataset):
+        assert info.format_channel_id(dataset.channels[59]) == '59.*'
+        assert info.format_channel_id(dataset.channels[59][0]) == '59.0'
 
-    def test_format_channel_id(self):
-        dataset = importFile(IDE_FILENAME)
-        self.assertEqual(info.format_channel_id(dataset.channels[59]), '59.*')
-        self.assertEqual(info.format_channel_id(dataset.channels[59][0]), '59.0')
-
-        self.assertEqual(info.format_channel_id(None), "None")
+        assert info.format_channel_id(None) == "None"
 
 
     def test_format_timedelta(self):
         # Note: only the start of strings is checked in order to avoid
         # differences in selected number of significant digits
         td = timedelta(seconds=0)
-        self.assertTrue(info.format_timedelta(td).startswith('00:00.'))
+        assert info.format_timedelta(td).startswith('00:00.')
 
         td = timedelta(seconds=1623430749.8969631)
-        self.assertTrue(info.format_timedelta(td).startswith('18789d 16:59:09.'))
+        assert info.format_timedelta(td).startswith('18789d 16:59:09.')
 
         # Number instead of timedelta. Unlikely but not not impossible.
-        self.assertTrue(info.format_timedelta(100000000).startswith('01:40.'))
-        self.assertEqual(info.format_timedelta(None), "None")
+        assert info.format_timedelta(100000000).startswith('01:40.')
+        assert info.format_timedelta(None) == "None"
 
 
     def test_format_timestamp(self):
         for i in range(0, 10000, 123):
-            self.assertTrue(info.format_timestamp(i).startswith(str(i)))
-            self.assertTrue(info.format_timestamp(str(i)).startswith(str(i)))
+            assert info.format_timestamp(i).startswith(str(i))
+            assert info.format_timestamp(str(i)).startswith(str(i))
 
-        self.assertEqual(info.format_timestamp('bogus'), 'bogus')
-        self.assertEqual(info.format_timestamp(None), "None")
+        assert info.format_timestamp('bogus') == 'bogus'
+        assert info.format_timestamp(None) == "None"
 
 
-class ChannelTableTests(unittest.TestCase):
+class TestChannelTable:
     """ Test the "channel table" generating functionality
     """
-    def setUp(self):
-        self.dataset = importFile(IDE_FILENAME)
-
-    def test_get_channel_table(self):
+    def test_get_channel_table(self, dataset):
         # TODO: Implement additional get_channel_table() tests?
-        ct = info.get_channel_table(self.dataset)
+        ct = info.get_channel_table(dataset)
 
-        self.assertEqual(len(ct.data), len(self.dataset.getPlots()),
-                         "Length of table's data did not match number of subchannels in IDE")
+        assert len(ct.data) == len(dataset.getPlots()), \
+               "Length of table's data did not match number of subchannels in IDE"
 
 
-    def test_channel_table_ranges(self):
+    def test_channel_table_ranges(self, dataset):
         # Exclude channels with extremely low sample rates
         # TODO: change acceptable delta based on sample rate
         MIN_RATE = 10  # Hz
-        DELTA = 0.01  # seconds
 
         # Test with a specified start
-        ct1 = info.get_channel_table(self.dataset, start="2s")
+        ct1 = info.get_channel_table(dataset, start="2s")
         for n, t in enumerate(ct1.data['start']):
             if ct1.data['rate'][n] < MIN_RATE:
                 continue
-            self.assertAlmostEqual(t / 10**6, 2, delta=DELTA, msg=f"{ct1.data['channel'][n]}")
+            assert pytest.approx(t / 10**6, 2), f"{ct1.data['channel'][n]}"
 
         # Test with a specified end
-        ct2 = info.get_channel_table(self.dataset, end="10s")
+        ct2 = info.get_channel_table(dataset, end="10s")
         for n, t in enumerate(ct2.data['end']):
             if ct1.data['rate'][n] < MIN_RATE:
                 continue
-            self.assertAlmostEqual(t / 10**6, 10, delta=DELTA, msg=f"{ct2.data['channel'][n]}")
+            assert pytest.approx(t / 10**6, 10), f"{ct2.data['channel'][n]}"
 
         # Test with both start and end specified
-        ct3 = info.get_channel_table(self.dataset, start="2s", end="10s")
-        self.assertListEqual(list(ct3.data['start']), list(ct1.data['start']))
-        self.assertListEqual(list(ct3.data['end']), list(ct2.data['end']))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        ct3 = info.get_channel_table(dataset, start="2s", end="10s")
+        assert list(ct3.data['start']) == list(ct1.data['start'])
+        assert list(ct3.data['end']) == list(ct2.data['end'])
--- tests/test_measurement.py
+++ tests/test_measurement.py
@@ -1,129 +1,124 @@
 import os.path
-import unittest
 
+import pytest
 from idelib.importer import importFile
 from endaq.ide import measurement
 
 
-IDE_FILENAME = os.path.join(os.path.dirname(__file__), "test.ide")
+@pytest.fixture
+def dataset():
+    with importFile(os.path.join("tests", "test.ide")) as ds:
+        yield ds
 
 
-class MeasurementTypeTests(unittest.TestCase):
+@pytest.fixture
+def types():
+    return [m for m in measurement.__dict__.values() if isinstance(m, measurement.MeasurementType)]
+
+
+class TestMeasurementTypes:
     """ Basic tests of the MeasurementType class and constant instances.
     """
-
-    def setUp(self):
-        self.types = [m for m in measurement.__dict__.values() if isinstance(m, measurement.MeasurementType)]
-
 
     def test_uniqueness(self):
         # Trying to instantiate a duplicate MeasurementType should return the original instance
         FOO = measurement.MeasurementType("Foo", "foo")
         BAR = measurement.MeasurementType("Foo", "foo")
-        self.assertEqual(FOO, BAR)
-        self.assertIs(FOO, BAR)
+        assert FOO == BAR
+        assert FOO is BAR
 
         # Again, this time testing against one predefined in the module
         ACCEL = measurement.MeasurementType("Acceleration", "acc")
-        self.assertEqual(ACCEL, measurement.ACCELERATION)
-        self.assertIs(ACCEL,  measurement.ACCELERATION)
+        assert ACCEL == measurement.ACCELERATION
+        assert ACCEL is measurement.ACCELERATION
 
 
-    def test_comp(self):
+    def test_comp(self, types):
         # Test that all predefined MeasurementTypes are equal to their string equivalents
-        for mt in self.types:
-            self.assertEqual(mt, str(mt), f"{mt!r} not equal to string '{mt}'")
+        for mt in types:
+            assert mt == str(mt), f"{mt!r} not equal to string '{mt}'"
 
 
     def test_strings(self):
         # Adding MeasurementTypes concatenates their strings
-        self.assertEqual(measurement.ACCELERATION + measurement.PRESSURE,
-                         f"{measurement.ACCELERATION} {measurement.PRESSURE}")
+        assert (measurement.ACCELERATION + measurement.PRESSURE
+                == f"{measurement.ACCELERATION} {measurement.PRESSURE}")
 
         # Negating creates a string prefixed by ``"-"``
-        self.assertEqual(-measurement.ACCELERATION,
-                         f"-{measurement.ACCELERATION}")
+        assert -measurement.ACCELERATION == f"-{measurement.ACCELERATION}"
 
-        self.assertEqual(measurement.ACCELERATION - measurement.PRESSURE,
-                         f"{measurement.ACCELERATION} -{measurement.PRESSURE}")
-
+        assert (measurement.ACCELERATION - measurement.PRESSURE
+                == f"{measurement.ACCELERATION} -{measurement.PRESSURE}")
 
 
-class GetByTypeTests(unittest.TestCase):
+
+class TestGetByType:
     """ Test the functions that retrieve `Channel` and/or `SubChannel` objects by
         measurement type from an IDE file (`idelib.dataset.Dataset`).
     """
 
-    def setUp(self):
-        self.dataset = importFile(IDE_FILENAME)
-
-
-    def test_get_measurement_type(self):
-        self.assertEqual(measurement.get_measurement_type(self.dataset.channels[32][0]),
-                         measurement.ACCELERATION)
-        self.assertListEqual(measurement.get_measurement_type(self.dataset.channels[80]),
-                            [measurement.ACCELERATION]*3)
+    def test_get_measurement_type(self, dataset):
+        assert (measurement.get_measurement_type(dataset.channels[32][0])
+                == measurement.ACCELERATION)
+        assert (measurement.get_measurement_type(dataset.channels[80])
+                == [measurement.ACCELERATION]*3)
 
 
     def test_split_types(self):
         inc, exc = measurement.split_types("*")
-        self.assertEqual(len(measurement.MeasurementType.types), len(inc))
-        self.assertEqual(len(exc), 0)
+        assert len(measurement.MeasurementType.types) == len(inc)
+        assert len(exc) == 0
 
         inc, exc = measurement.split_types(measurement.ACCELERATION + measurement.PRESSURE - measurement.LIGHT)
-        self.assertIn(measurement.ACCELERATION, inc)
-        self.assertIn(measurement.PRESSURE, inc)
-        self.assertIn(measurement.LIGHT, exc)
-        self.assertTrue(measurement.LIGHT not in inc)
-        self.assertTrue(measurement.PRESSURE not in exc)
+        assert measurement.ACCELERATION in inc
+        assert measurement.PRESSURE in inc
+        assert measurement.LIGHT in exc
+        assert measurement.LIGHT not in inc
+        assert measurement.PRESSURE not in exc
 
 
-    def test_filter_channels(self):
+    def test_filter_channels(self, dataset):
         """ Test test_filter_channels() filtering of Channels (filter applies
             if any SubChannel matches).
         """
-        channels = self.dataset.channels
+        channels = dataset.channels
         everything = measurement.filter_channels(channels)
-        self.assertListEqual(everything, measurement.filter_channels(list(channels.values())),
-                             "filter_channels(list) did not match filter_channels(dict)")
+        assert everything == measurement.filter_channels(list(channels.values())), \
+            "filter_channels(list) did not match filter_channels(dict)"
 
         accels = measurement.filter_channels(channels, measurement.ACCELERATION)
-        self.assertEqual(len(accels), 2)
+        assert len(accels) == 2
 
         noaccel = measurement.filter_channels(channels, -measurement.ACCELERATION)
-        self.assertEqual(len(noaccel), len(everything) - len(accels))
+        assert len(noaccel) == len(everything) - len(accels)
 
 
-    def test_filter_channels_subchannels(self):
+    def test_filter_channels_subchannels(self, dataset):
         """ Test test_filter_channels() filtering of SubChannels.
         """
-        subchannels = self.dataset.getPlots()
+        subchannels = dataset.getPlots()
         everything = measurement.filter_channels(subchannels)
 
         accels = measurement.filter_channels(subchannels, measurement.ACCELERATION)
-        self.assertEqual(len(accels), 6)
+        assert len(accels) == 6
 
         noaccel = measurement.filter_channels(subchannels, -measurement.ACCELERATION)
-        self.assertEqual(len(noaccel), len(everything) - len(accels))
+        assert len(noaccel) == len(everything) - len(accels)
 
 
-    def test_get_channels(self):
-        everything = measurement.get_channels(self.dataset)
+    def test_get_channels(self, dataset):
+        everything = measurement.get_channels(dataset)
 
-        accels = measurement.get_channels(self.dataset, measurement.ACCELERATION)
-        self.assertEqual(len(accels), 6)
+        accels = measurement.get_channels(dataset, measurement.ACCELERATION)
+        assert len(accels) == 6
 
-        noaccel = measurement.get_channels(self.dataset, -measurement.ACCELERATION)
-        self.assertEqual(len(noaccel), len(everything) - len(accels))
+        noaccel = measurement.get_channels(dataset, -measurement.ACCELERATION)
+        assert len(noaccel) == len(everything) - len(accels)
 
-        everything = measurement.get_channels(self.dataset, subchannels=False)
+        everything = measurement.get_channels(dataset, subchannels=False)
 
-        accels = measurement.get_channels(self.dataset, measurement.ACCELERATION, subchannels=False)
-        self.assertEqual(len(accels), 2)
+        accels = measurement.get_channels(dataset, measurement.ACCELERATION, subchannels=False)
+        assert len(accels) == 2
 
-        noaccel = measurement.get_channels(self.dataset, -measurement.ACCELERATION, subchannels=False)
-        self.assertEqual(len(noaccel), len(everything) - len(accels))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        noaccel = measurement.get_channels(dataset, -measurement.ACCELERATION, subchannels=False)
+        assert len(noaccel) == len(everything) - len(accels)
